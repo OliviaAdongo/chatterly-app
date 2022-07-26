73066a08e18333bcf7ba00d359c1a7bdf3052b33
Olivia Adongo <akinyioliviatove@gmail.com>
2022-07-26 12:06:47 +0300

    Using io.on to receive incoming data from client

--- server/index.js
+++ server/index.js
@@ -6,11 +6,14 @@ const cors = require ('cors');
 app.use(cors())
 app.use(express.json())  //allow send data from frontend to backend
 
-const server = app.listen('3001', ()=> console.log('Server running on port 3001!!!'));
+const server = app.listen('3001', ()=> console.log('Server running on port 3001!!!')); 
+//This is the cde for starting and listenning to the server connection to our server
 
 io = socket(server)
+// The connection argument represents incoming socket from a client
 io.on('connection', (socket)=>{
-    console.log(socket.id)
+    console.log(socket.id)// Random id given to whoever logs into the application
+    //data represents the room we are creating 
     socket.on('join_room', (data)=>{
         socket.join(data)
         console.log('User joined room' + data);
