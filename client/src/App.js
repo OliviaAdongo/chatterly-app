c3928df6e13e92a1b58dcde718dfc9bef990b20a
Olivia Adongo <akinyioliviatove@gmail.com>
2022-07-26 09:54:31 +0300

    Css edited

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,44 @@
+import  React, { useEffect, useState } from "react";
 import './App.css';
+import io from  'socket.io-client';
+
+
+let socket;
+const CONNECTION_PORT= 'localhost:3001/' //link for the backend
 
 function App() {
+  const [loggedIn, setLoggedIn]= useState(false)
+  const [room, setRoom]=useState('')
+  const [userName, setUserName]=useState('')
+
+  useEffect(()=>{
+    socket = io(CONNECTION_PORT)
+  }, [CONNECTION_PORT]);
+
+const connectToRoom= ()=>{
+  setLoggedIn=(true)
+  socket.emit('join_room', room)
+} 
+
   return (
     <div className="App">
-    
+    <h1>Chatterly</h1>
+    {!loggedIn ? (
+     <div className="logIn">
+        <div className="input">
+          <input type="text" placeholder="Name..." 
+          onChange={(e)=> {setUserName(e.target.value)}}></input>
+          <input type="room" placeholder="Room..." 
+          onChange={(e)=> {setRoom(e.target.value)}}></input>
+        </div>
+        <button onClick={connectToRoom}>Enter</button>
+     </div>
+    ): (
+      <div className="chatContainer">
+
+      </div>
+      
+    )}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,27 +1,22 @@
-const express = require("express");
+const express = require ('express');
+const socket =require('socket.io')
 const app = express();
-const http = require("http");
-const cors = require("cors");
-app.use(cors());
+const cors = require ('cors');
 
+app.use(cors())
+app.use(express.json())  //allow send data from frontend to backend
 
-io.on("connection", (socket) => {
-    console.log(`User Connected: ${socket.id}`);
-  
-    socket.on("join_room", (data) => {
-      socket.join(data);
-      console.log(`User with ID: ${socket.id} joined room: ${data}`);
-    });
-  
-    socket.on("send_message", (data) => {
-      socket.to(data.room).emit("receive_message", data);
-    });
-  
-    socket.on("disconnect", () => {
-      console.log("User Disconnected", socket.id);
-    });
-  });
-  
-  server.listen(3001, () => {
-    console.log("SERVER RUNNING");
-  });
+const server = app.listen('3001', ()=> console.log('Server running!!!'));
+
+io = socket(server)
+io.on('connection', (socket)=>{
+    console.log(socket.id)
+    socket.on('join_room', (data)=>{
+        socket.join(data)
+        console.log('User joined room' + data);
+    })
+
+    socket.on('disconnect', ()=> {
+        console.log('USER DISCONNECTED')
+    })
+})
